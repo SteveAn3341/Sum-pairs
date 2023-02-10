b03c958c18365f380914d49c6f656e5bdcc792a8
YOUR_NAME <steve3341@DESKTOP-DEMPQ84>
2023-02-09 21:29:40 -0500

    sumjs

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,23 @@
-exports.sumPairs = function() {
+// use a nested for loop to check if any pari of sum is equal to the given sum
 
-};
+
+
+
+// exports.sumPairs = function arraypair(array,sum){
+  function sum(array,sum ){
+    let arr = []
+      for (i = 0;i < array.length;i++) {
+        let first = array[i];
+          for (j = i + 1;j < array.length;j++) {
+            let second = array[j];
+              if ((first + second) == sum) {
+                  arr.push(first,second)  
+                }
+      }
+
+    }
+  return arr
+}
+console.log(sum([1, 2, 3, 4, 5], 6))
+
+
